31c60f7b320c2bb5eae9819d7cc4ce0c501e8aba
Manuel Valls Fernández <manolo@chatiku.com>
2014-11-02 10:01:51 +0100

    Fixes #2 - Add more array tests

--- test/Array.js
+++ test/Array.js
@@ -1,4 +1,36 @@
-var test = require('ebjs/common-test');
+var etest = require('ebjs/common-test'),
+    test = require('vz.test'),
+    walk = require('vz.walk');
+
 require('../main');
 
-module.exports = test([[1,2,3]],true);
+module.exports = walk(function*(){
+  
+  yield test('Simple arrays',function*(){
+    
+    yield etest([
+      [1,2,3,4,5],
+      ['foo','bar',6,7,8],
+      ['a','b','c','d','e','f']
+    ],true);
+    
+  });
+  
+  yield test('Nested arrays',function*(){
+    
+    yield etest([[1,2,3,4],[5,6,7,8],[9,10,11,12]],true);
+    
+  });
+  
+  yield test('Backreferences',function*(){
+    var arr = [],arr2;
+    
+    arr.push(arr2 = [arr]);
+    arr.push(arr);
+    arr.push(arr2);
+    
+    yield etest([arr],true);
+  });
+  
+});
+
